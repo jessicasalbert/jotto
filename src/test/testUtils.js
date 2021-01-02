b6a308f0de772cae8784cec00f3a4601333ceba0
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-02 09:06:06 -0500

    Add ShallowWrapper util

---
+++ src/test/testUtils.js
@@ -0,0 +1,10 @@
+
+/**
+ * Return node(s) with given data-test attribute
+ * @param  {ShallowWrapper} wrapper - Enzyme shallow wrapper
+ * @param  {string} val - value of data-test attribute 
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test=${val}]`);
+}
