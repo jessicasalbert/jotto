2ef149b9ff8841db2e1df5e189dd31905937af8d
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-02 09:14:09 -0500

    Setup tests for Congrats

--- src/Congrats/__test__/congrats.test.js
+++ src/Congrats/__test__/congrats.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+import { findByTestAttr } from '../../test/testUtils';
 import Congrats from '../congrats'
 
 Enzyme.configure( { adapter: new EnzymeAdapter() });
@@ -18,13 +19,19 @@ const setup = (props={}) => {
 }
 
 test('renders without error', () => {
-
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.length).toBe(1)
 })
 
 test('renders no text winner is false', () => {
-    
+    const wrapper = setup( {success : false });
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.text()).toBe('');
 })
 
 test('renders text when winner is successful', () => {
-    
+    const wrapper = setup( {success : true} );
+    const message = findByTestAttr(wrapper, 'congrats-message');
+    expect(message.text().length).not.toBe(0)
 })
--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -6,5 +6,5 @@
  * @returns {ShallowWrapper}
  */
 export const findByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test=${val}]`);
+    return wrapper.find(`[data-test='${val}']`);
 }
