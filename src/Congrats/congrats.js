5bcbad98f8bfbe5486b79c5440249b351d20b6e0
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-02 09:21:16 -0500

    Congrats component - tests passing

--- src/Congrats/congrats.js
+++ src/Congrats/congrats.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-export default () => {
-    return (
-        <div></div>
-    )
+
+/**
+ * @function
+ * @param  {object} props - React props
+ * @returns {JSX.element} - Rendered component, or 'null' if success prop is false
+ */
+
+export default (props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                Congratulations! You guessed the word.
+                </span>
+            </div>
+        )
+    } else {
+        return (
+            <div data-test="component-congrats"></div>
+        )
+    }
 }
