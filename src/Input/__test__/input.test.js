b3d79258bbe4b25024c442c94d25aedf0ddd805e
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-03 09:09:12 -0500

    Setup tests for Input component"

---
+++ src/Input/__test__/input.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../../test/testUtils';
+import Input from '../Input';
+
+/**
+ * @function setup
+ * @param  {object} initialState={} 
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState={}) => {
+    const wrapper = shallow(<Input />)
+    console.log(wrapper.debug())
+}
+
+setup();
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+
+        test('renders inut box', () => {
+
+        });
+
+        test('renders submit button', () => {
+
+        })
+    })
+
+    describe('word has been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+
+        test('does not render input box', () => {
+
+        });
+
+        test('does not render submit button', () => {
+
+        })
+    })
+
+})
+
+describe('update state', () => {
+
+})
